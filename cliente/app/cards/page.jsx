3450d3741d6c950a07bc76fac94ef563acbcb4a9
Nicolas <nicolasgfalabella@gmail.com>
2024-06-23 21:55:43 -0300

    Arreglos Domingo

--- cliente/app/cards/page.jsx
+++ cliente/app/cards/page.jsx
@@ -4,6 +4,7 @@ import React , {useEffect} from 'react'
 import { useRouter } from 'next/navigation'
 
 const CardPage = () => {
+  let numero = 5 ;
   const { user , isAuthenticated , loading } = useAuth()
   const router = useRouter()
 
